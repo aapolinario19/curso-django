ff89f2982ab46d0877762c6bfb55eb30d53a36f5
Alan Apolinario Matos <aapolinario19@gmail.com>
2021-12-02 14:18:14 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function gritar() {
+	alert("Ahhhhhhhhhhhhhhh!");
+} 
+
+function perguntar() {
+	var nome;
+	nome = prompt("Qual seu nome? ");
+	alert("Olá " + nome);
+}
+
+function mudar_texto() {
+	var h1 = document.getElementsByTagName("h1");
+
+	if(h1[0].innerText == "Geek University") {
+		h1[0].innerText = "Evolua seu lado Geek!";
+	}else{
+		h1[0].innerText = "Geek University";
+	}
+}
+
+function incrementar() {
+	var p1 = document.getElementById("p1");
+
+	p1.innerText = parseInt(p1.innerText) + 1;
+}
